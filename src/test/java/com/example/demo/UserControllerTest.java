dad8fe75a6b487838c52de7acc73e76911509e90
Martin Villegas <118768668+MartinBersh@users.noreply.github.com>
2024-11-12 13:30:25 -0500

    prueba 10

--- src/test/java/com/example/demo/UserControllerTest.java
+++ src/test/java/com/example/demo/UserControllerTest.java
@@ -6,6 +6,7 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
@@ -15,9 +16,11 @@ import static org.hamcrest.Matchers.equalTo;
 @ActiveProfiles("test")
 public class UserControllerTest {
 
+    @LocalServerPort
+    private int port;
     @BeforeEach
     void setUp() {
-        RestAssured.baseURI = "http://localhost:8080";
+        RestAssured.port = port;
     }
 
     @Test
